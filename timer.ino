8fd31e17db5d374f1693e4356a7c6ef86e8550a1
Kainos-SU <kapinos4@gmail.com>
2021-04-07 23:57:05 +0300

    Add miliseconds

--- timer.ino
+++ timer.ino
@@ -2,16 +2,14 @@
 #include <Arduino.h>
 #include <stdio.h>
 
-char time_format[] = "%d%d:%d%d:%d%d";
+char time_format[] = "%02d:%02d:%02d:%02d";
 char time_data[sizeof (time_format) / sizeof(char)];
 
 struct time_numbers {
-  byte seconds_low: 4;
-  byte seconds_high: 3;
-  byte minutes_low: 4;
-  byte minutes_high: 4;
-  byte hours_low: 4;
-  byte hours_high: 4;
+  unsigned int miliseconds: 10;
+  unsigned int seconds: 6;
+  unsigned int minutes: 6;
+  unsigned int hours: 7;
 };
 
 struct timer {
@@ -32,7 +30,7 @@ struct timer {
 struct timer * init_timer (int n) {
   struct timer *t = (struct timer *) malloc(sizeof(struct timer) * n);
   for (int i = 0; i < n; ++i) {
-    *t = (struct timer){(struct time_numbers){0,0,0,0,0,0}, 0, 1, 0};
+    *t = (struct timer){(struct time_numbers){0,0,0,0}, 0, 1, 0};
     ++t;
   }
   return t - n; 
@@ -41,22 +39,18 @@ struct timer * init_timer (int n) {
 void timer_update (struct timer *t) {
   t->changed = 1;
   struct time_numbers *temp = &(t->data);
-  ++temp->seconds_low;
-  if (temp->seconds_low == 10){
-    temp->seconds_low = 0;
-    ++temp->seconds_high;
-    if (temp->seconds_high == 6) {
-      temp->seconds_high = 0;
-      ++temp->minutes_low;
-      if (temp->minutes_low == 10) {
-        temp->minutes_low = 0;
-        ++temp->minutes_high;
-        if (temp->minutes_high == 6) {
-          temp->minutes_high = 0;
-          ++temp->hours_low;
-          if (temp->hours_low == 9) {
-            t->mode = 2;
-          }
+  ++temp->miliseconds;
+  if (temp->miliseconds == 1000) {
+    temp->miliseconds = 0;
+    ++(temp->seconds);
+    if (temp->seconds == 60) {
+      temp->seconds = 0;
+      ++(temp->minutes);
+      if (temp->minutes == 60) {
+        temp->minutes = 0;
+        ++(temp->hours);
+        if (temp->hours == 90) {
+          timer->mode = 2;
         }
       }
     }
@@ -67,7 +61,7 @@ void timer_tick(struct timer *t) {
   if (t->mode == 1) {
     timer_update(t);
   }else if (t->mode == 3) {
-    t->data = (struct time_numbers) {0,0,0,0,0,0};
+    t->data = (struct time_numbers) {0,0,0,0};
     t->mode = 0;
   }
 }
@@ -85,7 +79,7 @@ void timer_start (struct timer *t) {
 char *getTime (struct timer *t){
   if (t->changed == 1) {
     struct time_numbers *data = &(t->data);
-    sprintf (time_data, time_format, data->hours_high, data->hours_low, data->minutes_high, data->minutes_low, data->seconds_high, data->seconds_low);
+    sprintf (time_data, time_format, data->hours, data->minutes, data->seconds, data->miliseconds / 10);
     return time_data;
   }
   else
